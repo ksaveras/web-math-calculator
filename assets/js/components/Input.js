94a0b05b58b5f7ceaa53f964384f4993cd64a00f
Ksaveras Sakys <xawiers@gmail.com>
2020-09-18 16:28:14 +0100

    Use form submit handler in input

--- assets/js/components/Input.js
+++ assets/js/components/Input.js
@@ -3,30 +3,28 @@ import PropTypes from "prop-types";
 
 const Input = (props) => {
   const [expression, setExpression] = useState("");
-  const clickHandler = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
     props.clickHandler(expression);
     setExpression("");
   };
 
   return (
-    <div className="form-row">
-      <div className="form-group col-md-9">
-        <input
-          type="text"
-          className="form-control form-control-lg"
-          value={expression}
-          onChange={(event) => setExpression(event.target.value)}
-        />
+    <form onSubmit={submitHandler}>
+      <div className="form-row">
+        <div className="form-group col-md-9">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            value={expression}
+            onChange={(event) => setExpression(event.target.value)}
+          />
+        </div>
+        <div className="form-group col-md-3">
+          <button className="btn btn-lg btn-block btn-primary">=</button>
+        </div>
       </div>
-      <div className="form-group col-md-3">
-        <button
-          className="btn btn-lg btn-block btn-primary"
-          onClick={clickHandler}
-        >
-          =
-        </button>
-      </div>
-    </div>
+    </form>
   );
 };
 
